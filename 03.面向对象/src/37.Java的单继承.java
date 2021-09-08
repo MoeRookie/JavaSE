ad09088e9ce46652df26f928137f85027bf3f103
MoeRookie <MoeRookie@163.com>
2021-09-08 17:21:23 +0800

    三、面向对象

---
+++ 03.面向对象/src/37.Java的单继承.java
@@ -0,0 +1,31 @@
+/*
+一个类最多只能有一个直接的父类。但是有多个间接的父类。
+
+java是单继承。
+
+*/
+
+class Ye{
+
+	String name;
+
+}
+
+class Fu extends Ye{
+
+}
+
+//
+class  Zi extends Fu{
+
+}
+
+class Demo6 
+{
+	public static void main(String[] args) 
+	{
+	//	System.out.println("Hello World!");
+		Zi zi =new Zi();
+		zi.name = "狗娃";
+	}
+}
