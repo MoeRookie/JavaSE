fd0c3c3c19127ecfd4c299b61cddb65b57e46820
MoeRookie <MoeRookie@163.com>
2022-01-21 17:34:40 +0800

    十四、网络编程

---
+++ 13.网络编程/src/06.发起群聊端.java
@@ -0,0 +1,15 @@
+package cn.itcast.udp;
+
+public class ChatMain {
+	
+	public static void main(String[] args) {
+		ChatReceive chatReceive = new ChatReceive();
+		chatReceive.start();
+		
+		ChatSender chatSender = new ChatSender();
+		chatSender.start();
+		
+		
+	}
+
+}
